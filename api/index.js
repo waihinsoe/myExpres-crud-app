7ff9abfcc3a65de2bc025cff6d1d50e0269769a1
waihinsoe <waihinsoe332004@gmail.com>
2023-02-15 18:56:29 +0630

    fifth commit

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const formidable = require("formidable");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+const aws = require("aws-sdk");
+const fs = require("fs");
 const app = express();
+
+const users = [];
+
 dotenv.config();
 const apiUrl = process.env.API_URL;
-console.log(apiUrl);
 
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
 const html = `<!DOCTYPE html>
@@ -30,6 +38,46 @@ app.get("/api", (req, res) => {
 
 app.get("/api/users", (req, res) => {
   res.send({ message: "This is user" });
+});
+
+app.get("/api/upload", (req, res) => {
+  res.json(users);
+});
+
+app.post("/api/upload", (req, res) => {
+  const form = formidable({ multiples: true });
+  form.parse(req, (err, fields, files) => {
+    if (err) throw err;
+    console.log(err, fields, files);
+    const userName = fields.name;
+    const fileObj = files.files;
+    const fileName = uuidv4() + fileObj.originalFilename;
+    const filePath = fileObj.filepath;
+    const filestream = fs.createReadStream(filePath);
+
+    const spacesEndpoint = new aws.Endpoint("sgp1.digitaloceanspaces.com");
+    const s3 = new aws.S3({
+      endpoint: spacesEndpoint,
+      accessKeyId: "DO00AGWGBAWFVGJKUVTX",
+      secretAccessKey: "83ckcc4GSag+spdD345p1kZ//5rCY0DWk9OZJ4AE03U",
+    });
+
+    s3.upload(
+      {
+        Bucket: "msquarefdc",
+        Key: fileName,
+        ACL: "public-read",
+        Body: filestream,
+      },
+      (err, data) => {
+        if (err) return console.log(err);
+        const imgUrl = data.Location;
+        const user = { name: userName, url: imgUrl };
+        users.push(user);
+        return res.end();
+      }
+    );
+  });
 });
 
 app.listen(9000, () => {
--- public/script.js
+++ public/script.js
@@ -9,3 +9,52 @@ const fetchData = async () => {
   }
 };
 fetchData();
+
+//showUser-start
+const userDiv = document.querySelector(".userDiv");
+
+const showUserInFrontEnd = (data) => {
+  userDiv.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    const user = document.createElement("div");
+    user.classList.add("user");
+    user.innerHTML = `<img src="${data[i].url}" alt="" />
+                      <h3>${data[i].name}</h3>`;
+
+    userDiv.append(user);
+  }
+};
+//showUser-end
+
+// getData-start
+const getData = async () => {
+  const respone = await fetch(`${apiUrl}/upload`);
+  const data = await respone.json();
+  showUserInFrontEnd(data);
+};
+// getData-end
+
+// formdata upload start
+
+const formTag = document.querySelector("form");
+const inputFile = document.querySelector("#inputFile");
+const inputName = document.querySelector("#inputName");
+const formData = new FormData();
+
+formTag.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const files = [...inputFile.files];
+  files.forEach((file) => {
+    formData.append("files", file);
+  });
+
+  formData.append("name", inputName.value);
+  const respone = await fetch(`${apiUrl}/upload`, {
+    method: "POST",
+    body: formData,
+  });
+  getData();
+});
+getData();
+
+// formData upload end
